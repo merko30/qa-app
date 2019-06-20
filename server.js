b2f177898056bb07287b8edf92550e0142472477
merko30 <merk030@hotmail.com>
2019-06-20 12:22:00 +0200

    update app

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ app.use("/api", router);
 app.use(errorHandler);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use("/uploads", express.static("./uploads"));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 app.listen(PORT, () => {
