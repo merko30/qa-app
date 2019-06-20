63b655c4cd962060713e276161cbcdb90917993b
merko30 <merk030@hotmail.com>
2019-06-20 12:30:28 +0200

    update app

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(errorHandler);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use("/uploads", express.static("./uploads"));
   app.use(express.static("client/build"));
+  app.use("/uploads", express.static(__dirname, "uploads"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
